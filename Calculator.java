ab6bd283a637bf9f05f6fab59b08a48769cb29ca
배영호 <baeyeongho@baeyeonghoui-MacBookAir.local>
2022-11-28 20:52:58 +0900

    test

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+package git_practice1;
+
+public class Calculator {
+	// 계산기 
+	public int add(int a, int b, int c) {
+		
+		return a+b+c;
+	}
+	
+	public int substract(int a, int b) {
+		return a-b;
+	}
+}
